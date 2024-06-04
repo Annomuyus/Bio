ec628137e52da73055bc3a53cde16a90dbc3ab29
Annomuyus <148428217+Annomuyus@users.noreply.github.com>
2024-06-04 15:14:32 +0700

    Add files via upload

---
+++ Febio/koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+$localhost="localhost";
+$username="root";
+$pass="";
+$database="kasir";
+
+$conn=mysqli_connect($localhost,$username,$database);
+
+?>
---
+++ Febio/register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<body>
+<form action="image.png">
+  <div class="container">
+  <link rel="stylesheet" href="register.css">
+    <h1>Register</h1>
+
+    <label for="username"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="username" id="username" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required>
+    <hr>
+
+    <button type="submit" class="registerbtn">Register</button>
+  </div>
+  
+  <div class="container signin">
+  </div>
+</form>
+
+</body>
+</html>
